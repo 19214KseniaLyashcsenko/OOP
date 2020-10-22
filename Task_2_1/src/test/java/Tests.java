088814fbbb3f3f1e3f32c16a8c6cc147e8b8030b
19214KseniaLyashchenko <55023695+19214KseniaLyashcsenko@users.noreply.github.com>
2020-10-22 16:28:02 +0700

    Update Tests.java

--- Task_2_1/src/test/java/Tests.java
+++ Task_2_1/src/test/java/Tests.java
@@ -1,10 +1,11 @@
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import java.util.NoSuchElementException;
 
 public class Tests {
 
     @Test
-    public void intTypeTests() {
+    public void simpleTests() {
         Stack mStack = new Stack();
         mStack.push(79);
         mStack.push(35);
@@ -14,84 +15,48 @@ public class Tests {
 
     @Test
     public void differentTypeTests() {
-        Stack mStack = new Stack();
-        mStack.push(79);
-        mStack.push("Hello World");
-        mStack.push(54.34f);
-        mStack.push('F');
-        mStack.push(false);
-        Assertions.assertEquals(mStack.count(), 5);
-        Assertions.assertEquals(mStack.pop(), false);
+        Stack<Integer> intStack = new Stack<Integer>();
+        intStack.push(79);
+        intStack.push(1234);
+        intStack.push(54);
+        intStack.push(0);
+        intStack.push(12);
+        Assertions.assertEquals(intStack.count(), 5);
+        Assertions.assertEquals(intStack.pop(), 12);
+
+        Stack<String> stringStack = new Stack<String>();
+        stringStack.push("Hello");
+        stringStack.push("World");
+        stringStack.push("!!");
+        stringStack.push("AAAAA");
+        stringStack.push("Fish");
+        Assertions.assertEquals(stringStack.count(), 5);
+        Assertions.assertEquals(stringStack.pop(), "Fish");
+
+        Stack<Boolean> boolStack = new Stack<Boolean>();
+        boolStack.push(true);
+        boolStack.push(false);
+        Assertions.assertEquals(boolStack.count(), 2);
+        Assertions.assertEquals(boolStack.pop(), false);
     }
-    
-    @Test
+
+    @Test(expected = NoSuchElementException.class)
     public void emptyStack() {
         Stack mStack = new Stack();
-        Assertions.assertEquals(mStack.pop(), null);
+        mStack.pop();
     }
 
     @Test
     public void bigTests() {
-        Stack mStack = new Stack();
-        mStack.push(79);
-        mStack.push("Hello");
-        mStack.push(35);
-        mStack.push(66);
-        mStack.push("Hello World");
-        mStack.push(5);
-        mStack.push(1);
-        mStack.push('F');
-        mStack.push(false);
-        mStack.push(79);
-        mStack.push("Hello");
-        mStack.push(35);
-        mStack.push(66);
-        mStack.push("Hello World");
-        mStack.push(5);
-        mStack.push(1);
-        mStack.push('F');
-        mStack.push(false);
-        mStack.push(79);
-        mStack.push("Hello");
-        mStack.push(35);
-        mStack.push(66);
-        mStack.push("Hello World");
-        mStack.push(5);
-        mStack.push(1);
-        mStack.push('F');
-        mStack.push(false);
-        mStack.push(79);
-        mStack.push("Hello");
-        mStack.push(35);
-        mStack.push(66);
-        mStack.push("Hello World");
-        mStack.push(5);
-        mStack.push(1);
-        mStack.push('F');
-        mStack.push(false);
-        mStack.push(79);
-        mStack.push("Hello");
-        mStack.push(35);
-        mStack.push(66);
-        mStack.push("Hello World");
-        mStack.push(5);
-        mStack.push(1);
-        mStack.push('F');
-        mStack.push(false);
-        mStack.push(79);
-        mStack.push("Hello");
-        mStack.push(35);
-        mStack.push(66);
-        mStack.push("Hello World");
-        mStack.push(5);
-        mStack.push(1.453f);
-        mStack.push('F');
-        mStack.push("bla bla bla");
-        Assertions.assertEquals(mStack.count(), 54);
-        Assertions.assertEquals(mStack.pop(), "bla bla bla");
-        Assertions.assertEquals(mStack.count(), 53);
-        Assertions.assertEquals(mStack.pop(), 'F');
-        Assertions.assertEquals(mStack.pop(), 1.453f);
-        Assertions.assertEquals(mStack.count(), 51);
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int i = 0; i < 100000; i++){
+            stack.push(i);
+        }
+        Assertions.assertEquals(stack.count(),100000);
+        for(int i = 100000 - 1 ; i >= 0; i--){
+            Assertions.assertEquals(stack.pop(), i);
+        }
+        Assertions.assertEquals(stack.count(),0);
     }
+
 }
