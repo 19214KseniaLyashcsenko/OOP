19a9087f8bc4fc75b5088171f2a863ef628a723e
19214KseniaLyashchenko <55023695+19214KseniaLyashcsenko@users.noreply.github.com>
2020-11-13 16:24:32 +0700

    Update SubstringSearch.java

--- Task_1_2/src/main/java/SubstringSearch.java
+++ Task_1_2/src/main/java/SubstringSearch.java
@@ -1,85 +1,73 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
-
 
 public class SubstringSearch {
 
-    static long[] resArr = new long[0];
-    static int top = 0;
-    static int k = 0;
-
     /**
-     *Увеличиваем массив
-     */
-    public static void increaseArr(){
-        resArr = Arrays.copyOf(resArr, resArr.length + 1);
-        top++;
-    }
-
-    /**
-     *
      * @param inStream - stream of input
      * @param substrIn - substring to find in string
      * @return - array with the indices of all occurrences of the substring in the string
      * @throws IOException - exception handler
      */
-    public static long[] substringSearch(InputStream inStream, String substrIn) throws IOException {
 
-        char substr[] = new char[substrIn.length()];
+    public static ArrayList<Long> substringSearch(InputStream inStream, String substrIn) throws IOException {
+        if (inStream == null) {
+            throw new IllegalArgumentException("no file");
+        }
+        if (substrIn == null) {
+            throw new IllegalArgumentException("No substring");
+        }
+
+        ArrayList<Long> resArr = new ArrayList<>();
+
+        char substr[];
         substr = substrIn.toCharArray();
+        int k = 10;
         char str[] = new char[substr.length * 3];
 
-        BufferedReader readIn = new BufferedReader(new InputStreamReader(inStream, "UTF-8"));
+        try (BufferedReader readIn = new BufferedReader(new InputStreamReader(inStream, "UTF-8"))) {
+            System.arraycopy(substr, 0, str, 0, substr.length);
+            int charRead = readIn.read(str, substr.length, substr.length * 2);
 
-        int k1 = readIn.read(str, substr.length, substr.length);
-        if (k1 < substr.length) {
-            return resArr;
-        }
-        System.arraycopy(substr, 0, str, 0, substr.length);
-        int k2 = readIn.read(str, substr.length * 2, substr.length);
-
-        if ((k2 == -1) && (Arrays.equals(str, substr.length, substr.length * 2, substr, 0, substr.length))) { // when substring equals to string for search
-            if(k == top) increaseArr();
-            resArr[k++] = 0;
-            return resArr;
-        }
-
-        long pos = 0;
-
-        while (k2 != -1) {
-
-            int z[] = new int[str.length];
-            z[0] = 0;
-            int l = 0, r = 0;
-            for (int i = 1; i < str.length; i++) {
-                if (i <= r) {
-                    z[i] = Math.min(r - i + 1, z[i - l]);
-                }
-
-                while (i + z[i] < str.length && (str[z[i]] == str[i + z[i]])) {
-                    ++z[i];
-                }
-
-                if (i + z[i] - 1 > r) {
-                    l = i;
-                    r = i + z[i] - 1;
-                }
+            if ((charRead == -1) && (Arrays.equals(str, substr.length, substr.length * 2, substr, 0, substr.length))) {
+                resArr.add(0L);
+                return resArr;
             }
 
-            for (int i = substr.length; i < str.length; i++) {
-                if (z[i] >= substr.length && !(Arrays.asList(resArr).contains(pos))) {
-                    if(k == top) increaseArr();
-                    resArr[k++] = pos;
-                }
-                pos++;
-            }
+            long posStr = 0;  
+            charRead = substr.length;
+            while (charRead != -1) {
 
-            k1 = k2;
-            pos -= k2;
-            System.arraycopy(str, substr.length * 2, str, substr.length, substr.length);
-            k2 = readIn.read(str, substr.length * 2, substr.length);
-            if (k2 < substr.length) {
-                str[substr.length * 2 + k2] = 0;
+                int z[] = new int[str.length];
+                z[0] = 0;
+                int l = 0, r = 0;
+                for (int i = 1; i < str.length; ++i) {
+                    if (i <= r) {
+                        z[i] = Math.min(r - i + 1, z[i - l]);
+                    }
+
+                    while (i + z[i] < str.length && (str[z[i]] == str[i + z[i]])) {
+                        ++z[i];
+                    }
+
+                    if (i + z[i] - 1 > r) {
+                        l = i;
+                        r = i + z[i] - 1;
+                    }
+                }
+
+                for (int i = substr.length; i < str.length; i++) {
+                    if (z[i] >= substr.length && !(Arrays.asList(resArr).contains(posStr))) {
+                        resArr.add(posStr);
+                        System.out.println(posStr);
+                    }
+                    posStr++;
+                }
+
+                posStr -= charRead;
+                System.arraycopy(str, substr.length * 2, str, substr.length, substr.length);
+                charRead = readIn.read(str, substr.length * 2, substr.length);
             }
         }
         return resArr;
