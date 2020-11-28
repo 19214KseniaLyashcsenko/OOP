9ae5d4b96f135d11e22c628d31db7e0c978a0e37
19214KseniaLyashchenko <55023695+19214KseniaLyashcsenko@users.noreply.github.com>
2020-11-28 12:41:16 +0700

    Update Tests.java

--- Task_3_1/src/test/java/Tests.java
+++ Task_3_1/src/test/java/Tests.java
@@ -7,8 +7,8 @@ public class Tests {
     public void Test1(){
         MyDate date = new MyDate(26, "November", 2020);
         MyDate res;
-        res = MyCalendar.plusDays(date, 1024);
-        String dayOfWeek = MyCalendar.dayOfWeek(res);
+        res =  MyDate.plusDays(date, 1024);
+        String dayOfWeek =  MyDate.dayOfWeek(res);
         assertEquals(dayOfWeek,"Saturday");
 
     }
@@ -17,11 +17,14 @@ public class Tests {
     public void Test2() {
         MyDate date1 = new MyDate(26, "November", 2020);
         MyDate date2 = new MyDate(9, "May", 1945);
+        System.out.printf("%d %s %d\n",date1.getYear(), date1.getMonth(), date1.getDay());
+        //System.out.printf("%d %d %d\n",year2, month1, day1);
         int[] resArr;
-        resArr = MyCalendar.minDate(date1, date2);
+        resArr =  MyDate.subtractDate(date1, date2);
         int year = resArr[0];
         int month = resArr[1];
         int day = resArr[2];
+        System.out.printf("%d %d %d", year, month,day);
         assertEquals(75, year);
         assertEquals(6, month);
         assertEquals(17,day);
@@ -30,24 +33,22 @@ public class Tests {
     @Test
     public void Test3() {
         MyDate date = new MyDate(6, "February", 2001);
-        String res = MyCalendar.dayOfWeek(date);
+        String res =  MyDate.dayOfWeek(date);
         assertEquals("Tuesday", res);
 
         date = new MyDate(29, "February", 2020);
-        res = MyCalendar.dayOfWeek(date);
+        res =  MyDate.dayOfWeek(date);
         assertEquals("Saturday", res);
     }
 
     @Test
     public void Test4(){
         MyDate date = new MyDate(26, "November", 2020);
-        MyDate res = MyCalendar.plusDays(date, 119);
+        MyDate res =  MyDate.plusDays(date, 119);
         assertEquals(res.getMonth(), "March");
     }
 
 }
-
-
 
 
 
