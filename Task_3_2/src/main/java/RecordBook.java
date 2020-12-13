e5ca946c33de9f9d96a396a87039c82693c7dc33
19214KseniaLyashchenko <55023695+19214KseniaLyashcsenko@users.noreply.github.com>
2020-12-13 17:47:37 +0700

    Update RecordBook.java

--- Task_3_2/src/main/java/RecordBook.java
+++ Task_3_2/src/main/java/RecordBook.java
@@ -1,50 +1,54 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 public class RecordBook {
 
-    private static ArrayList<String> arrSubjects = new ArrayList<String>(Collections.nCopies(100, ""));
-    private static ArrayList<Integer> arrGrades = new ArrayList<Integer>(Collections.nCopies(100, 0));
+    private  ArrayList<String> arrSubjects = new ArrayList<String>(Collections.nCopies(100, ""));
+    private  ArrayList<Integer> arrGrades = new ArrayList<Integer>(Collections.nCopies(100, 0));
 
-    static int[] semester = new int[]{0, 0, 10, 20, 30, 40, 50, 60, 70};
-    static String[] strSemester = new String[]{"","Семестр 1","Семестр 2","Семестр 3","Семестр 4","Семестр 5", "Семестр 6", "Семестр 7","Семестр 8"};
-    static boolean isThree = false;
-    static boolean[] isFiveInSem = new boolean[]{true,true,true,true,true,true,true,true,true};
-    static int lastSem = 0;
-    static int counter = 0;
-    static int counterFive = 0;
+    private  int[] semester = new int[]{0, 0, 10, 20, 30, 40, 50, 60, 70};
+    private  boolean isThree = false;
+    private  boolean[] isFiveInSem = new boolean[]{true,true,true,true,true,true,true,true,true};
+    private  int lastSem = 0;
+    private  int counter = 0;
+    private  int counterFive = 0;
+
+    private String name;
+    private String surname;
+
+    RecordBook(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
 
     /**
      * adds the subject and its grade to the student's record
-     * @param semester number of semester
+     * @param numSemester number of semester
      * @param subject examination subject
      * @param grade grade for the subject on the exam
      */
-    public static void semesterAdd(String semester, String subject, int grade) {
-        int i;
-        if(Arrays.asList(strSemester).contains(semester)) {
-            i = Arrays.asList(strSemester).indexOf(semester);
+    public  void semesterAdd(int numSemester, String subject, int grade) { ;
+        if(numSemester > 8 || numSemester < 1) {
+            throw new IllegalArgumentException("Semester entered incorrectly!!!");
         }
-        else throw new IllegalArgumentException("Semester entered incorrectly!!!");
         if(grade > 5 && grade < 1) throw new IllegalArgumentException("Grade entered incorrectly!!!");
 
-        if (RecordBook.semester[i] < RecordBook.semester[i - 1]) RecordBook.semester[i] = RecordBook.semester[i - 1];
-        arrSubjects.add(RecordBook.semester[i], subject);
-        arrGrades.add(RecordBook.semester[i], grade);
-        RecordBook.semester[i]++;
+        if (semester[numSemester] < semester[numSemester - 1]) semester[numSemester] = semester[numSemester - 1];
+        arrSubjects.add(semester[numSemester], subject);
+        arrGrades.add(semester[numSemester], grade);
+        semester[numSemester]++;
         counter++;
         if (grade == 3) isThree = true;
         if (grade == 5) counterFive++;
-        else isFiveInSem[i] = false;
-        if (lastSem < i) lastSem = i;
+        else isFiveInSem[numSemester] = false;
+        if (lastSem < numSemester) lastSem = numSemester;
     }
 
     /**
      * calculates the arithmetic mean of all grades
      * @return
      */
-    public static double averageGrade() {
+    public double averageGrade() {
         int sum = 0;
         for (int i = 0; i < arrGrades.size(); i++) {
             sum += (int) arrGrades.get(i);
@@ -58,12 +62,12 @@ public class RecordBook {
      * calculates whether it is possible to increase the scholarship
      * @return
      */
-    public static boolean isIncreaseGrant() {
+    public boolean isIncreaseGrant() {
         if(lastSem != 1){
             if(isFiveInSem[lastSem] == true && isFiveInSem[lastSem - 1] == true) return true;
             return false;
         }
-        else System.out.println("Для повышения стипендии нужно проучиться два семестра без троек");
+        else System.out.println("You haven't studied enough to raise your scholarship");
         return false;
     }
 
@@ -71,13 +75,11 @@ public class RecordBook {
      * calculates whether it is possible to get a red diploma
      * @return
      */
-    public static boolean isRedDiploma(){
-        if (lastSem != 8) System.out.println("Вам еще далеко до диплома)");
-        if(((float)counterFive/counter * 100 >=75) && arrGrades.get(semester[8] - 1) == 5 && isThree == false) return true;
+    public boolean isRedDiploma(){
+        int eightSem = 8;
+        int threshold = 75;
+        if (lastSem != eightSem) System.out.println("You are still far from red diploma)");
+        if(((float)counterFive/counter * 100 >=threshold) && arrGrades.get(semester[8] - 1) == 5 && isThree == false) return true;
         return false;
     }
-
 }
-
-
-
