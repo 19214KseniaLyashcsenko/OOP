d3c8a1b945d389f5883621a37e44230bdfba3da8
19214KseniaLyashchenko <55023695+19214KseniaLyashcsenko@users.noreply.github.com>
2021-01-17 15:02:08 +0700

    Update Tests.java

--- Task_3_2/src/test/java/Tests.java
+++ Task_3_2/src/test/java/Tests.java
@@ -1,169 +1,109 @@
 import org.junit.Test;
+import java.util.ArrayList;
+import static org.junit.Assert.*;
 
-import static org.junit.Assert.assertEquals;
-
-public class Tests {
-
+public class Tests{
     @Test
-    public void Test1() {
-        RecordBook RecordBook = new RecordBook("Ксения", "Лященко");
-        RecordBook.semesterAdd(1, "Императивное программирование", 4);
-        RecordBook.semesterAdd(1, "Введение в алгебру и анализ", 3);
-        RecordBook.semesterAdd(1, "Введение в дискретную математику и математическую логику", 4);
-        RecordBook.semesterAdd(1, "Основы культуры речи", 5);
-        RecordBook.semesterAdd(1, "Декларативное программирование", 4);
-        RecordBook.semesterAdd(1, "История", 5);
-
-        RecordBook.semesterAdd(2, "Императивное программирование", 4);
-        RecordBook.semesterAdd(2, "Цифровые платформы", 5);
-        RecordBook.semesterAdd(2, "Введение в алгебру и анализ", 4);
-        RecordBook.semesterAdd(2, "Иностранный язык", 4);
-        RecordBook.semesterAdd(2, "Введение в дискретную математику и математическую логику", 5);
-        RecordBook.semesterAdd(2, "Декларативное программирование", 4);
-        RecordBook.semesterAdd(2, "Дипломная работа", 5);
-
-        assertEquals(4.3, RecordBook.averageGrade(), 1);
-        assertEquals(false,RecordBook.isIncreaseGrant());
-        assertEquals(false, RecordBook.isRedDiploma());
+    public void Test1(){
+        ArrayList<RecordBook.Rec> sem1 = new ArrayList<RecordBook.Rec>();
+        sem1.add(new RecordBook.Rec("Введение в алгебру и анализ", 3));
+        sem1.add(new RecordBook.Rec("Декларативное программирование", 4));
+        sem1.add(new RecordBook.Rec("Императивное программирование",  4));
+        sem1.add(new RecordBook.Rec("История",  5));
+        sem1.add(new RecordBook.Rec("Основы культуры речи",5  ));
+        sem1.add(new RecordBook.Rec("Введение в дискретную математику и математическую логику",  4));
+        ArrayList<RecordBook.Rec> sem2 = new ArrayList<RecordBook.Rec>();
+        sem2.add(new RecordBook.Rec("Введение в алгебру и анализ", 4));
+        sem2.add(new RecordBook.Rec("Декларативное программирование",4));
+        sem2.add(new RecordBook.Rec("Императивное программирование",  4));
+        sem2.add(new RecordBook.Rec("Иностранный язык",  4));
+        sem2.add(new RecordBook.Rec("Цифровые платформы", 5));
+        sem2.add(new RecordBook.Rec("Введение в дискретную математику и математическую логику", 4));
+        RecordBook book = new RecordBook(3, "Ксения", "Лященко");
+        book.replaceSemester(1,sem1);
+        book.replaceSemester(2,sem2);
+        assertEquals(4.3, book.averageGrade(),1);
+        assertEquals(false,book.isIncreaseGrant());
+        assertEquals(false,book.isRedDiploma());
     }
 
     @Test
     public void Test2(){
-        RecordBook RecordBook = new RecordBook("Анатолий", "Буравов");
-        RecordBook.semesterAdd(1, "Императивное программирование", 5);
-        RecordBook.semesterAdd(1, "Введение в алгебру и анализ", 5);
-        RecordBook.semesterAdd(1, "Введение в дискретную математику и математическую логику", 5);
-        RecordBook.semesterAdd(1, "Основы культуры речи", 5);
-        RecordBook.semesterAdd(1, "Декларативное программирование", 5);
-        RecordBook.semesterAdd(1, "История", 5);
-
-        RecordBook.semesterAdd(2, "Императивное программирование", 5);
-        RecordBook.semesterAdd(2, "Цифровые платформы", 5);
-        RecordBook.semesterAdd(2, "Введение в алгебру и анализ", 5);
-        RecordBook.semesterAdd(2, "Иностранный язык", 5);
-        RecordBook.semesterAdd(2, "Введение в дискретную математику и математическую логику", 5);
-        RecordBook.semesterAdd(2, "Декларативное программирование", 5);
-        RecordBook.semesterAdd(2, "Дипломная работа", 5);
-
-        assertEquals(5, RecordBook.averageGrade(), 1);
-        assertEquals(true,RecordBook.isIncreaseGrant());
-        assertEquals(false, RecordBook.isRedDiploma());
+        ArrayList<RecordBook.Rec> sem1 = new ArrayList<RecordBook.Rec>();
+        sem1.add(new RecordBook.Rec("Введение в алгебру и анализ", 5 ));
+        sem1.add(new RecordBook.Rec("Декларативное программирование",5  ));
+        sem1.add(new RecordBook.Rec("Императивное программирование",  5));
+        sem1.add(new RecordBook.Rec("История",  5));
+        sem1.add(new RecordBook.Rec("Основы культуры речи",5  ));
+        sem1.add(new RecordBook.Rec("Введение в дискретную математику и математическую логику", 5));
+        ArrayList<RecordBook.Rec> sem2 = new ArrayList<RecordBook.Rec>();
+        sem2.add(new RecordBook.Rec("Введение в алгебру и анализ",  5));
+        sem2.add(new RecordBook.Rec("Декларативное программирование",  5));
+        sem2.add(new RecordBook.Rec("Императивное программирование",  5));
+        sem2.add(new RecordBook.Rec("Иностранный язык",  5));
+        sem2.add(new RecordBook.Rec("Цифровые платформы", 5 ));
+        sem2.add(new RecordBook.Rec("Введение в дискретную математику и математическую логику", 5 ));
+        RecordBook book = new RecordBook(3, "Александр", "Забивной");
+        book.replaceSemester(1,sem1);
+        book.replaceSemester(2,sem2);
+        assertEquals(5, book.averageGrade(),1);
+        assertEquals(true,book.isIncreaseGrant());
+        assertEquals(true,book.isRedDiploma());
     }
 
     @Test
     public void Test3(){
-        RecordBook RecordBook = new RecordBook("Александр", "Забивной");
-        RecordBook.semesterAdd(1, "Императивное программирование", 5);
-        RecordBook.semesterAdd(1, "Введение в алгебру и анализ", 5);
-        RecordBook.semesterAdd(1, "Введение в дискретную математику и математическую логику", 5);
-        RecordBook.semesterAdd(1, "Основы культуры речи", 4);
-        RecordBook.semesterAdd(1, "Декларативное программирование", 5);
-        RecordBook.semesterAdd(1, "История", 5);
-
-        RecordBook.semesterAdd(2, "Императивное программирование", 5);
-        RecordBook.semesterAdd(2, "Цифровые платформы", 5);
-        RecordBook.semesterAdd(2, "Введение в алгебру и анализ", 5);
-        RecordBook.semesterAdd(2, "Иностранный язык", 4);
-        RecordBook.semesterAdd(2, "Введение в дискретную математику и математическую логику", 5);
-        RecordBook.semesterAdd(2, "Декларативное программирование", 5);
-        RecordBook.semesterAdd(2, "Дипломная работа", 5);
-
-        RecordBook.semesterAdd(3, "Введение в искусственный интеллект", 5);
-        RecordBook.semesterAdd(3, "Модели вычислений", 5);
-
-        RecordBook.semesterAdd(4, "Введение в компьютерные сети", 5);
-        RecordBook.semesterAdd(4, "Программируемые микроконтроллеры", 5);
-
-        RecordBook.semesterAdd(5, "Вычислительная математика", 4);
-        RecordBook.semesterAdd(5, "Введение в разработку мобильных приложений",5 );
-
-        RecordBook.semesterAdd(6, "Компьютерное моделирование",5 );
-        RecordBook.semesterAdd(6, "Правоведение", 5);
-
-        RecordBook.semesterAdd(7, "Инновационная экономика и технологическое предпринимательство",5 );
-        RecordBook.semesterAdd(7, "Распределенные алгоритмы",5 );
-
-        RecordBook.semesterAdd(8, "Основы риторики", 5);
-        RecordBook.semesterAdd(8, "Дипломная работа", 5);
-
-        assertEquals(4.8, RecordBook.averageGrade(), 1);
-        assertEquals(true,RecordBook.isIncreaseGrant());
-        assertEquals(true, RecordBook.isRedDiploma());
+        ArrayList<RecordBook.Rec> sem1 = new ArrayList<RecordBook.Rec>();
+        sem1.add(new RecordBook.Rec("Введение в алгебру и анализ", 5));
+        sem1.add(new RecordBook.Rec("Декларативное программирование",5  ));
+        sem1.add(new RecordBook.Rec("Императивное программирование", 5 ));
+        sem1.add(new RecordBook.Rec("История",  5));
+        sem1.add(new RecordBook.Rec("Основы культуры речи",5));
+        sem1.add(new RecordBook.Rec("Введение в дискретную математику и математическую логику",  5));
+        ArrayList<RecordBook.Rec> sem2 = new ArrayList<RecordBook.Rec>();
+        sem2.add(new RecordBook.Rec("Введение в алгебру и анализ",5));
+        sem2.add(new RecordBook.Rec("Декларативное программирование",5));
+        sem2.add(new RecordBook.Rec("Императивное программирование", 5));
+        sem2.add(new RecordBook.Rec("Иностранный язык",  5));
+        sem2.add(new RecordBook.Rec("Цифровые платформы", 5 ));
+        sem2.add(new RecordBook.Rec("Введение в дискретную математику и математическую логику", 5 ));
+        RecordBook book = new RecordBook(3,"Ольга", "Мирная");
+        book.qualifyingWork(3);
+        book.replaceSemester(1,sem1);
+        book.replaceSemester(2,sem2);
+        assertEquals(5, book.averageGrade(),1);
+        assertEquals(true,book.isIncreaseGrant());
+        assertEquals(false,book.isRedDiploma());
     }
 
     @Test
     public void Test4(){
-        RecordBook RecordBook = new RecordBook("Ольга", "Мирная");
-        RecordBook.semesterAdd(1, "Императивное программирование", 5);
-        RecordBook.semesterAdd(1, "Введение в алгебру и анализ", 5);
-        RecordBook.semesterAdd(1, "Введение в дискретную математику и математическую логику", 5);
-        RecordBook.semesterAdd(1, "Основы культуры речи", 4);
-        RecordBook.semesterAdd(1, "Декларативное программирование", 5);
-        RecordBook.semesterAdd(1, "История", 5);
-
-        RecordBook.semesterAdd(2, "Императивное программирование", 5);
-        RecordBook.semesterAdd(2, "Цифровые платформы", 5);
-        RecordBook.semesterAdd(2, "Введение в алгебру и анализ", 5);
-        RecordBook.semesterAdd(2, "Иностранный язык", 4);
-        RecordBook.semesterAdd(2, "Введение в дискретную математику и математическую логику", 5);
-        RecordBook.semesterAdd(2, "Декларативное программирование", 5);
-        RecordBook.semesterAdd(2, "Дипломная работа", 5);
-
-        RecordBook.semesterAdd(3, "Введение в искусственный интеллект", 3);
-        RecordBook.semesterAdd(3, "Модели вычислений", 5);
-
-        RecordBook.semesterAdd(4, "Введение в компьютерные сети", 5);
-        RecordBook.semesterAdd(4, "Программируемые микроконтроллеры", 5);
-
-        RecordBook.semesterAdd(5, "Вычислительная математика", 4);
-        RecordBook.semesterAdd(5, "Введение в разработку мобильных приложений",5 );
-
-        RecordBook.semesterAdd(6, "Компьютерное моделирование",5 );
-        RecordBook.semesterAdd(6, "Правоведение", 5);
-
-        RecordBook.semesterAdd(7, "Инновационная экономика и технологическое предпринимательство",5 );
-        RecordBook.semesterAdd(7, "Распределенные алгоритмы",5 );
-
-        RecordBook.semesterAdd(8, "Основы риторики", 5);
-        RecordBook.semesterAdd(8, "Дипломная работа", 5);
-
-        assertEquals(4.8, RecordBook.averageGrade(), 1);
-        assertEquals(true,RecordBook.isIncreaseGrant());
-        assertEquals(false, RecordBook.isRedDiploma());
-    }
-
-    @Test
-    public void Test5(){
-        RecordBook RecordBook = new RecordBook("Сергей", "Лютый");
-        RecordBook.semesterAdd(1, "Введение в дискретную математику и математическую логику", 5);
-        RecordBook.semesterAdd(1, "Основы культуры речи", 4);
-        RecordBook.semesterAdd(8, "Основы риторики", 5);
-        RecordBook.semesterAdd(7, "Распределенные алгоритмы",5 );
-        RecordBook.semesterAdd(1, "Декларативное программирование", 5);
-        RecordBook.semesterAdd(1, "История", 5);
-        RecordBook.semesterAdd(2, "Императивное программирование", 5);
-        RecordBook.semesterAdd(2, "Цифровые платформы", 5);
-        RecordBook.semesterAdd(1, "Императивное программирование", 5);
-        RecordBook.semesterAdd(1, "Введение в алгебру и анализ", 5);
-        RecordBook.semesterAdd(2, "Введение в алгебру и анализ", 5);
-        RecordBook.semesterAdd(6, "Компьютерное моделирование",5 );
-        RecordBook.semesterAdd(2, "Иностранный язык", 4);
-        RecordBook.semesterAdd(2, "Дипломная работа", 5);
-        RecordBook.semesterAdd(8, "Дипломная работа", 5);
-        RecordBook.semesterAdd(4, "Введение в компьютерные сети", 5);
-        RecordBook.semesterAdd(4, "Программируемые микроконтроллеры", 5);
-        RecordBook.semesterAdd(5, "Вычислительная математика", 4);
-        RecordBook.semesterAdd(5, "Введение в разработку мобильных приложений",5 );
-        RecordBook.semesterAdd(3, "Введение в искусственный интеллект", 3);
-        RecordBook.semesterAdd(3, "Модели вычислений", 5);
-        RecordBook.semesterAdd(6, "Правоведение", 5);
-        RecordBook.semesterAdd(7, "Инновационная экономика и технологическое предпринимательство",5 );
-        RecordBook.semesterAdd(2, "Введение в дискретную математику и математическую логику", 5);
-        RecordBook.semesterAdd(2, "Декларативное программирование", 5);
-
-
-        assertEquals(4.8, RecordBook.averageGrade(), 1);
-        assertEquals(true,RecordBook.isIncreaseGrant());
-        assertEquals(false, RecordBook.isRedDiploma());
+        ArrayList<RecordBook.Rec> sem1 = new ArrayList<RecordBook.Rec>();
+        sem1.add(new RecordBook.Rec("Введение в алгебру и анализ",  5));
+        sem1.add(new RecordBook.Rec("Декларативное программирование",5  ));
+        sem1.add(new RecordBook.Rec("Императивное программирование",  5));
+        sem1.add(new RecordBook.Rec("История",  5));
+        sem1.add(new RecordBook.Rec("Основы культуры речи",5));
+        sem1.add(new RecordBook.Rec("Введение в дискретную математику и математическую логику",5));
+        ArrayList<RecordBook.Rec> sem2 = new ArrayList<RecordBook.Rec>();
+        sem2.add(new RecordBook.Rec("Введение в алгебру и анализ",5));
+        sem2.add(new RecordBook.Rec("Декларативное программирование",5  ));
+        sem2.add(new RecordBook.Rec("Императивное программирование",  5));
+        sem2.add(new RecordBook.Rec("Иностранный язык",  5));
+        sem2.add(new RecordBook.Rec("Цифровые платформы", 5 ));
+        sem2.add(new RecordBook.Rec("Введение в дискретную математику и математическую логику",5  ));
+        RecordBook book = new RecordBook(4,"Сергей", "Лютый");
+        book.replaceSemester(1,sem1);
+        book.replaceSemester(2,sem2);
+        assertEquals(5, book.averageGrade(),1);
+        book.addRecordToSemester(3,new RecordBook.Rec("Модели вычислений",4));
+        book.addRecordToSemester(3,new RecordBook.Rec("ДУ и ТФКП0",  4));
+        assertEquals(4.6, book.averageGrade(),1);
+        book.addRecordToSemester(3,new RecordBook.Rec("ТВ и МС", 4));
+        book.addRecordToSemester(3,new RecordBook.Rec("Операционные системы", 5));
+        book.addRecordToSemester(3,new RecordBook.Rec("Введение в ИИ",5));
+        assertEquals(4.8, book.averageGrade(),1);
+        book.qualifyingWork( 5);
+        assertEquals(true,book.isRedDiploma());
     }
 }
