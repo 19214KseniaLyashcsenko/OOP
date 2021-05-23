ff191d4f87c76bdd67be33c451106be8d87d5377
KseniaLyashchenko <55023695+19214KseniaLyashcsenko@users.noreply.github.com>
2021-05-23 21:26:27 +0700

    Update WithoutThreads.java

--- Task2_2_1/src/main/java/WithoutThreads.java
+++ Task2_2_1/src/main/java/WithoutThreads.java
@@ -6,7 +6,7 @@ public class WithoutThreads{
     public Boolean isPrime(ArrayList<Integer> arrayNumbers, Predicate<Integer> predicate){
 
         for(int i = 0; i < arrayNumbers.size(); i++){
-            int number = arrayNumbers.get(i);
+            Integer number = arrayNumbers.get(i);
             for(int j = 0; j < Math.sqrt(number); j++)
                 if(predicate.test(number))
                     return true;
